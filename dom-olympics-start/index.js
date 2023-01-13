82e08d89cf5b8765b44d54d36ea264838540439c
tfelder <troyfelder14@yahoo.com>
2023-01-12 14:04:43 -1000

    Up to silver

---
+++ dom-olympics-start/index.js
@@ -0,0 +1,25 @@
+// Access the header element and add the required text
+let header = document.getElementById("header")
+header.innerHTML = "JavaScript made this!!<br><span>Troy</span> wrote this JavaScript"; 
+
+// Add the CSS style classes
+document.querySelector("#header").classList.add("header");
+document.querySelector("span").classList.add("name");
+
+// variable for the messages DIVs
+let msg = document.getElementsByClassName("message");
+
+// Change the conversation
+msg[0].textContent = "Will this work?";
+msg[1].textContent = "I hope so";
+msg[2].textContent = "yea I really hope so too";
+msg[3].textContent = "It Did!!!";
+
+// clears the chat
+let clear = document.getElementById("clear-button");
+clear.addEventListener("click", function(){
+    let msgs = document.getElementsByClassName("messages");
+    msgs[0].innerHTML = "";
+});
+
+// Change the theme
