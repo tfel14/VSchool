5be5fac460160550783240f70d979ab5a9425ee1
tfelder <troyfelder14@yahoo.com>
2023-01-17 13:30:34 -1000

    Finished delete button

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -1,11 +1,16 @@
 const form = document.addItem
+const remove = document.getElementById("list")
 
 
-form.addEventListener("submit", () => {
-    event.preventDefault();
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
     const newItem = form.title.value;
     const li = document.createElement("li")
-    li.innerHTML = "<div>"+ newItem +"</div><button>edit</button><button>X</button>"
+    li.innerHTML = `<div>${newItem}</div><button>edit</button><button>X</button>`
     document.getElementById("list").appendChild(li);
+    form.title.value = "";
 })
 
+remove.addEventListener("click", (e) => {
+    e.target.parentNode.remove()
+})
