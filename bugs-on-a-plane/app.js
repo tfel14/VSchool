e2f37859528890b6ecb2448b02e59d8a3a80a455
tfelder <troyfelder14@yahoo.com>
2023-03-16 18:58:08 -1000

    done

--- bugs-on-a-plane/app.js
+++ bugs-on-a-plane/app.js
@@ -1,27 +1,26 @@
-var form = document.getElementByID("arline-form");
-var submit = document.getElementByID(submit);
-var query = document.querySelector;
+const form = document.airlineForm;
+const submit = document.getElementById("submit");
+const query = document.querySelector;
 
-function formAlert{} {
-    var firstName = form.elements["firstName"].value;
-    var lastName = form.elements["lastName"].value;
-    var age = form.elements["age"].value;
-    var gender = form.elements["gender"].value;
-    var location = form.elements["travel-location"].value;
-    var diet = {};
-    if (form.elements['vegan'].checked) {
-        var diet.pop(document.getElementById("vegan").value);
+function formAlert(event) {
+    var firstName = form.firstName.value;
+    var lastName = form.lastName.value;
+    var age = form.age.value;
+    var gender = form.gender.value;
+    var location = form.travelLocation.value;
+    var diet = [];
+    if (form.vegan.checked) {
+         diet.push(form.vegan.value);
     }
-    if (form.elements['gluten'].checked) {
-        diet.push(document.getElementById('gluten').value);
+    if (form.gluten.checked) {
+        diet.push(form.gluten.value);
     }
-    if (form.elements['paleo'].checked) {
-        diet.push(document.getElementById('paleo').value);
+    if (form.paleo.checked) {
+        diet.push(form.paleo.value);
     }
 
 
     alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location + "\nDiet: " + diet + "\nAwesome, now if you die, it won't be an accident..");
 }
 
-
-submit.addEventListener("click", formalert);
+submit.addEventListener("click", formAlert);
